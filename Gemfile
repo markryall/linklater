12d1ce98d2e3cf262c6e7cc53d1a706f7ad7279c
Mark Ryall <mark@ryall.name>
2011-02-26 15:01:30 +1000

    rails application with one scaffold generated controller/model/migration for crud links

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'sqlite3'
+
+group :test, :development do
+  gem "rspec-rails", "~> 2.4"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linklater::Application.config.secret_token = '66f389a6a7b6697eb62e13e6776e1a0e541222ed595857b50d5e1c01dfb20050dabdb4e30feed9444028152d16c05be27acb224c2defe49a694932f9c12d1dbc'
